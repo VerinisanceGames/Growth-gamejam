785a63a8e000fb5285c3afb999421cc8eafaec6f
vladimir-stal <vladimirstal@gmail.com>
2026-03-17 15:45:47 +0300

    Second one

---
+++ Assets/Scripts/Game/Sound/MenuSounds.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menusounds : MonoBehaviour
+{
+    public AudioSource mainMenuMusicSource;
+    public AudioSource buttonSoundSource;
+    public AudioClip clickSound;
+    public AudioClip menuMusic;
+
+    private bool musicFadeOutEnabled = false;
+
+    void Start()
+    {
+        if (mainMenuMusicSource != null && mainMenuMusicSource != null)
+        {
+            //buttonSoundSource.PlayOneShot(menuMusic);
+            mainMenuMusicSource.Play();
+        }
+    }
+
+    public void PlayClickSound()
+    {
+        musicFadeOutEnabled = true;
+        
+        if (buttonSoundSource != null && buttonSoundSource != null)
+        {
+            buttonSoundSource.Play();
+        }
+
+        // if (mainMenuMusicSource != null && mainMenuMusicSource.isPlaying)
+        // {
+        //     mainMenuMusicSource.Stop();
+        // }
+        
+    }
+
+    // public void StopPlayback()
+    // {
+    //     if (mainMenuMusicSource != null && mainMenuMusicSource.isPlaying)
+    //     {
+    //         mainMenuMusicSource.Stop();
+    //     }
+    // }
+
+    public void Update()
+    {
+        if (musicFadeOutEnabled)
+        {
+            if (mainMenuMusicSource.volume <= 0.01f)
+            {
+                mainMenuMusicSource.Stop();
+                musicFadeOutEnabled = false;
+            }
+            else
+            {
+                float newVolume = mainMenuMusicSource.volume - (0.08f * Time.deltaTime);
+                if (newVolume < 0f)
+                {
+                    newVolume = 0f;
+                }
+                mainMenuMusicSource.volume = newVolume;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Tips.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tips : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        /** 
+            первый раз когда подходишь к растению, оно говорит фразу:
+            Мухоловка: "I'm hungry! Hungry!"
+            Цветок: "Good morning, Darling"
+            Гриб: "Z-z-z"
+            Джаспер: "Hello, sir. How is your day going?"
+        */
+
+        /** 
+            типы подсказок: 
+            voice - текст/голос
+            texture - картинка/надпись на бочке с удобрением 
+            reaction - реакция растений на приближение с удобрением
+
+            Fert 1. Гриб. Type: voice, texture.  Голос: "First one is for Lili. Hurry up. You have 1 minute". Texture: Бочка с белым крестом.
+            Fert 2. Цветок. Type: texture. Бочка с надписью "BAB"
+            Fert 3. Мухоловка. Type: reaction. Реакция(Flytrap): "Give it to meeeee"
+            Fert 4. Цветок. Type: reaction. Реакция: "Darling, give it to me, please"
+            Fert 5. Гриб. Type: texture. Бочка со знаком капля воды
+            Fert 6. Мухоловка. Type: voice, reaction. Голос: "This one is for Fred." Реакция(Flower): "Darling, I hope you're not going to fertilize me with this."
+            Fert 7. Мухоловка. Type: reaction. Реакция(Flytrap): "Give it to meeeee" (?)
+            Fert 8. Гриб. Type: texture. Texture: Бочка с белым крестом.
+            Fert 9. Цветок. Type: texture. Бочка с надписью "BAB"
+            Fert 10. Джаспер. Type: voice. Голос: "Last one. Poison. You know what to do."
+
+            + Джаспер всегда говорит, когда приходишь к нему с удобрением. Реакция: "Would you be so kind to feed me, sir?"
+
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
